476b2962012e378c4c07d2ac6e466fb9266ebd10
OrenWF <owfriedman@gmail.com>
2018-07-22 04:33:02 -0400

    ProgramEntity

--- edu.cuny.hunter.optionalrefactoring.core/src/edu/cuny/hunter/optionalrefactoring/core/refactorings/RefactoringContextSettings.java
+++ edu.cuny.hunter.optionalrefactoring.core/src/edu/cuny/hunter/optionalrefactoring/core/refactorings/RefactoringContextSettings.java
@@ -12,6 +12,10 @@ import org.eclipse.jdt.core.IMethod;
 
 import com.google.common.collect.Sets;
 
+/**
+ * @author <a href="mailto:ofriedman@acm.org">Oren Friedman</a>
+ *
+ */
 public class RefactoringContextSettings {
 	
 	private static enum ContextType {
@@ -28,7 +32,7 @@ public class RefactoringContextSettings {
 		return new RefactoringContextSettings(Sets.newHashSet(ContextType.FIELDS, 
 				ContextType.LOCAL_VARS, ContextType.METHOD_PARAMS, ContextType.METHOD_RETURNS));
 	}
-	
+
 	public static RefactoringContextSettings  of(final Map<String,String> choices) {
 		Set<ContextType> set = new LinkedHashSet<>();
 		for (String s : choices.keySet()) {
@@ -68,27 +72,26 @@ public class RefactoringContextSettings {
 		return settings.contains(ContextType.METHOD_RETURNS);
 	}
 	
-/*	TODO: implement necessary Utility Method isNotInitialized(IField)
- *  private boolean refactorsUninitializedFields() {
+	private boolean refactorsUninitializedFields() {
 		return settings.contains(ContextType.IMPLICIT_FIELDS);
-	}*/
+	}
 
-	public Predicate<IJavaElement> nonComplying = element -> {
-		if (element instanceof IMethod)
+	public Predicate<ProgramEntity> nonComplying = entity -> {
+		if (entity.element() instanceof IMethod)
 			return !this.refactorsMethodReturns();
 
-		if (element instanceof IField) {
+		if (entity.element() instanceof IField) {
 			if (!this.refactorsFields()) return true;
 
-			/* TODO: implement this static method
-			 * if (Util.isNotInitialized((IField)element))
-			 * 	return !this.refactorsUninitializedFields();
-			*/
+			
+			if (entity.implicitlyNull())
+				return !this.refactorsUninitializedFields();
+			
 		}
-		if (element instanceof ILocalVariable) {
+		if (entity instanceof ILocalVariable) {
 			if (!this.refactorsLocalVars()) return true;
 			
-			if (((ILocalVariable) element).isParameter())
+			if (((ILocalVariable) entity).isParameter())
 				return !this.refactorsMethodParams();
 		}
 		return false;
