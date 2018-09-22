9d87515c7061b6e42ff268ea84964dd4e64c96ff
Walee Ahmed <dev@walee.me>
2018-09-22 10:54:18 -0400

    Added a comment test case

---
+++ edu.cuny.hunter.optionalrefactoring.tests/resources/ConvertNullToOptional/testTransformationWithComment/out/A.java
@@ -0,0 +1,8 @@
+package p;
+
+import java.util.Optional;
+
+public class A {
+
+	Optional<Object> o = Optional.empty(); // this is my comment
+}
--- edu.cuny.hunter.optionalrefactoring.tests/test cases/edu/cuny/hunter/optionalrefactoring/ui/tests/ConvertNullToOptionalRefactoringTest.java
+++ edu.cuny.hunter.optionalrefactoring.tests/test cases/edu/cuny/hunter/optionalrefactoring/ui/tests/ConvertNullToOptionalRefactoringTest.java
@@ -403,6 +403,10 @@ public class ConvertNullToOptionalRefactoringTest extends RefactoringTest {
 	public void testTransformationTwoFields() throws Exception {
 		this.transformationHelper(null, new RefactoringStatus());
 	}
+	
+	public void testTransformationWithComment() throws Exception {
+		this.transformationHelper(null, new RefactoringStatus());
+	}
 
 	private void transformationHelper(Choices turnOff, RefactoringStatus expectedStatus) throws Exception {
 		ICompilationUnit icu = this.createCUfromTestFile(this.getPackageP(), "A");
